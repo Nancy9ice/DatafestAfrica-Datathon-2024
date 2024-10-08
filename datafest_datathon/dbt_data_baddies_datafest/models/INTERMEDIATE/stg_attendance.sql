3c5c0686724b28ccbfe7ad9d152ac10f12a4c59a
Nancy Amandi <103530451+Nancy9ice@users.noreply.github.com>
2024-10-08 15:00:08 +0100

    Delete datafest_datathon directory

--- datafest_datathon/dbt_data_baddies_datafest/models/INTERMEDIATE/stg_attendance.sql
+++
@@ -1,9 +0,0 @@
-SELECT DISTINCT
-    student_course_id,
-    total_minutes,
-    minutes_present,
-    created_at as attendance_created_at,
-    TO_CHAR(CAST(created_at AS DATE), 'YYYYMMDD') AS attendance_created_date_id,
-    updated_at as attendance_updated_at,
-    TO_CHAR(CAST(updated_at AS DATE), 'YYYYMMDD') AS attendance_updated_date_id
-FROM {{ source('data_baddies_datafest', 'raw_attendance_day') }}
--- datafest_datathon/dbt_data_baddies_datafest/models/INTERMEDIATE/stg_students_grades.sql
+++
@@ -1,5 +0,0 @@
-SELECT DISTINCT
-    student_course_id,
-    marking_period_id,
-    points as scores
-FROM {{ source('data_baddies_datafest', 'raw_gradebook_grades') }}
