d71457c7e1611197590a60171e081bccd68bf1f9
Nancy9ice <amandinancy16@gmail.com>
2024-10-08 03:42:35 +0100

    edited files

--- datafest_datathon/definitions.py
+++ datafest_datathon/definitions.py
@@ -3,6 +3,7 @@ from dagster import Definitions, repository
 from datafest_datathon.assets.airbyte_instance import airbyte_assets
 from datafest_datathon.assets.snowflake_dbt import my_dbt_assets, dbt_resource
 from datafest_datathon.constants import airbyte_instance
+from datafest_datathon.jobs import airbyte_dbt_sync_job
 from datafest_datathon.schedules import daily_airbyte_dbt_schedule  # Import your schedule
 
 # Define the Dagster repository
@@ -10,4 +11,5 @@ defs = Definitions(
     assets=airbyte_assets + [my_dbt_assets],  # Combine Airbyte and dbt assets
     schedules=[daily_airbyte_dbt_schedule],  # Add schedule
     resources={"airbyte": airbyte_instance, "dbt": dbt_resource},
+    jobs=[airbyte_dbt_sync_job]
     )
